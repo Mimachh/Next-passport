c7d9f5f6cc229047d78686d06e8329c505ea013a
Karl <106066885+Mimachh@users.noreply.github.com>
2023-03-21 16:23:00 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const db = mysql.createPool({
     connectionLimit: 10,
-    host: '109.234.161.47',
-    user: 'rzgf6652_admin',
-    password: 'Karl1991!',
-    database: 'rzgf6652_nextjs_auth',
+    host: '',
+    user: '',
+    password: '',
+    database: '',
 })
 
 module.exports = db;
